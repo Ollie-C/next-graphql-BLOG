9ac520a160e4161e5ffc84f01f5dc8ab6ceda743
Ollie Cross <iio.olivercross@gmail.com>
2023-01-21 18:46:36 +0000

    Basic styling

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -3,14 +3,25 @@ import styles from "../styles/BlogCard.module.css";
 
 const BlogPost = ({ title, author, coverPhoto, datePublished, slug }) => {
   return (
-    <div className={styles.card}>
+    <article className={styles.card}>
       <Link href={"/posts/" + slug}>
-        <h1>{title}</h1>
         <div className={styles.imgContainer}>
           <img src={coverPhoto.url} alt="" />
         </div>
       </Link>
-    </div>
+      <div className={styles.header}>
+        <h1>{title}</h1>
+        <div className={styles.details}>
+          <div className={styles.author}>
+            <img src={author.avatar.url} alt="" />
+            <h3>{author.username}</h3>
+          </div>
+          <div className={styles.date}>
+            <h3>{datePublished}</h3>
+          </div>
+        </div>
+      </div>
+    </article>
   );
 };
 
